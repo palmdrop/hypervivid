e71dd881b6cd2a7035c34b0a071b1b71a14ea74f
palmdrop <anton@exlex.se>
2022-08-10 17:15:05 +0200

    Add additional swamp mass variations

--- src/nodes/swamp-mass/swamp-mass.svelte
+++ src/nodes/swamp-mass/swamp-mass.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import SubstrateView from './../../components/node/templates/SubstrateView.svelte';
-  import programData from '../../assets/programs/swamp-mass.json';
+  import { randomElement } from '../../utils/random';
+
+  const programs = Object.values(
+    import.meta.globEager('../../assets/programs/swamp-mass/*')
+  ).map(module => module.default);
+
+  const program = randomElement(programs);
 </script>
 
 <SubstrateView 
-  programData={programData}
+  programData={program}
 />
