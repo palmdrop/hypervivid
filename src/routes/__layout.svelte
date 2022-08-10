23a8dff98be3fedfc3755d2515d7532e593b8fe6
palmdrop <anton@exlex.se>
2022-08-11 00:17:01 +0200

    Prevent meta from injecting tracking scripts when using in-app browsers

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,13 +1,31 @@
 <script lang="ts">
   import { title$ } from '$stores/head';
+  import { loading } from '$stores/loading';
+  import { navigating } from '$app/stores';
+  import { onMount } from 'svelte';
 
   import '../global.css';
   import '../fonts.css';
   import '../theme.css';
-  import { loading } from '$stores/loading';
-  import { navigating } from '$app/stores';
 
   $: $loading = !!$navigating;
+
+  onMount(() => {
+    /*
+      Override default addEventListener function to block instagram/facebook/meta from injecting selection listeners.
+      Please read more at https://krausefx.com/blog/ios-privacy-instagram-and-facebook-can-track-anything-you-do-on-any-website-in-their-in-app-browser
+    */
+    const addEventListener = document.addEventListener;
+    const newAddEventListener: typeof addEventListener = function (this: any, _, listener) {
+      if (listener.toString().indexOf("messageHandlers.fb_getSelection") > -1) {
+        return null;
+      }
+
+      return addEventListener.apply(this, arguments as any);
+    }
+
+    document.addEventListener = newAddEventListener;
+  });
 </script>
 
 <svelte:head>
@@ -17,3 +35,10 @@
 </svelte:head>
 
 <slot />
+
+<!-- 
+  These spans are inserted to trick instagram/facebook/meta to not inject tracking scripts on the page.
+  Please read more at https://krausefx.com/blog/ios-privacy-instagram-and-facebook-can-track-anything-you-do-on-any-website-in-their-in-app-browser
+-->
+<span id="iab-pcm-sdk" />
+<span id="iab-autofill-sdk" />
