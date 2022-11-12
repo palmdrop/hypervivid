39e7bbec835ff67b753e385685cc00a517398b93
palmdrop <anton@exlex.se>
2022-11-12 11:48:49 +0100

    Dumb build hack

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # git submodule foreach 'npm install'
-svelte-kit build
+# svelte-kit build
+# rm -r ./build
+cd ./src/modules/synthetic-grids
+npm install
+npm run build
+cd ../../../
+cp -r ./src/modules/synthetic-grids/dist ./build
