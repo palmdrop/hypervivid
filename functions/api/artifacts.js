1d05f152ff4c0eea5cf8b342ed34ef9f777464e1
palmdrop <anton@exlex.se>
2023-01-15 17:10:40 +0100

    Process data and add cors options

--- functions/api/artifacts.js
+++ functions/api/artifacts.js
@@ -1,3 +1,88 @@
+export const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  // 'Access-Control-Allow-Origin': 'https://palmdrop.github.io',
+  'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
+  'Access-Control-Max-Age': '86400',
+};
+
+const processData = (data) => {
+  // NOTE: matchall is slow, cache result and only redo it if document is stale!
+  // TODO: implement pagination! do not process all data unless needed. Cache pages
+  const entries = [...data.matchAll(entryParseRegex)]
+    .map(match => processMatch(match));
+
+  /*
+  // Is this faster?
+  const entries = data
+    .split("#")
+    .map(line => {
+      const match = line.match(entryParseRegex);
+      if(match) return processMatch(match);
+      return undefined;
+    })
+    .filter(Boolean);
+  */
+
+  const firstDate = entries.at(0).metadata.datetime;
+  const lastDate = entries.at(-1).metadata.datetime;
+
+  const numberOfDays = 1 + getTimeDifference(
+    firstDate, lastDate, 'days'
+  );
+
+  entries.forEach((entry, i) => {
+    const dayIndex = getTimeDifference(
+      entry.metadata.datetime, lastDate, 'days'
+    );
+
+    entry.dayIndex = dayIndex;
+    const day = timeline[dayIndex];
+    day.count++;
+    day.entryIndices.push(i);
+
+    if(day.datetime === '') {
+      day.datetime = entry.metadata.datetime.split('T')[0];
+    }
+  });
+
+  return {
+    entries,
+    numberOfDays
+  }
+}
+
+const processMatch = match => {
+  const [
+    ,
+    datetime,
+    year,
+    month,
+    day,
+    hour,
+    minute,
+    content,
+    note
+  ] = match;
+
+  const formattedDate = datetime.replace('T', ' ');
+
+  return {
+    metadata: {
+      datetime,
+      formattedDate,
+      year,
+      month,
+      day,
+      hour,
+      minute
+    },
+    content,
+    note,
+    dayIndex: -1
+  };
+}
+
+
 export const onRequestGet = async ( context ) => {
   const {
     // request,
@@ -18,10 +103,14 @@ export const onRequestGet = async ( context ) => {
 
     if(result.ok) {
       const data = await result.text();
+      const processedData = processData(data);
 
       return new Response(
-        data,
-        { status: 200 }
+        JSON.stringify(processedData),
+        { 
+          status: 200,
+          headers: CORS_HEADERS
+        }
       );
     } else {
       throw new Error();
@@ -33,7 +122,8 @@ export const onRequestGet = async ( context ) => {
       undefined,
       { 
         status: 500,
-        error: JSON.stringify(error)
+        error: JSON.stringify(error),
+        headers: CORS_HEADERS
       }
     )
   }
