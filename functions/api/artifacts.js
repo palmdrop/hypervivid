d5b7ccd1b0af69ac3cebb9b080ff1516386fcafb
palmdrop <anton@exlex.se>
2023-01-15 18:38:21 +0100

    Only allow requests from palmdrop.github.io

--- functions/api/artifacts.js
+++ functions/api/artifacts.js
@@ -1,6 +1,6 @@
 const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  // 'Access-Control-Allow-Origin': 'https://palmdrop.github.io',
+  // 'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://palmdrop.github.io',
   'Access-Control-Allow-Methods': 'GET,HEAD,POST,OPTIONS',
   'Access-Control-Max-Age': '86400',
 }; 
